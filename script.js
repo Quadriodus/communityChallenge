f8bd5a7ce4122d84b3990cc721a3528895c3e7fc
Quadri Odunuga <quadriodunuga@Quadris-MacBook-Pro.local>
2023-02-01 19:07:14 +0100

    i edited the web page

---
+++ script.js
@@ -0,0 +1,39 @@
+$('.count').each(function () {
+    $(this).prop('Counter',0).animate({
+        Counter: $(this).text()
+    }, {
+        duration: 4000,
+        easing: 'swing',
+        step: function (now) {
+            $(this).text(Math.ceil(now));
+        }
+    });
+});
+
+$(function(){  // $(document).ready shorthand
+    $('.monster').fadeIn('slow');
+  });
+  
+  $(document).ready(function() {
+      
+      /* Every time the window is scrolled ... */
+      $(window).scroll( function(){
+      
+          /* Check the location of each desired element */
+          $('.hideme').each( function(i){
+              
+              var bottom_of_object = $(this).position().top + $(this).outerHeight();
+              var bottom_of_window = $(window).scrollTop() + $(window).height();
+              
+              /* If the object is completely visible in the window, fade it it */
+              if( bottom_of_window > bottom_of_object ){
+                  
+                  $(this).animate({'opacity':'1'},1500);
+                      
+              }
+              
+          }); 
+      
+      });
+      
+  });
